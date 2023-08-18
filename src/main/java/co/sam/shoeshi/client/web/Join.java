181a01281e827acf6f904048b3d45ea6b9b2ee07
admin <admin@DESKTOP-1BUH4FE>
2023-08-18 11:58:41 +0900

    Merge branch 'master' of https://github.com/MyNameIsYGKim/shoeshi.git

--- src/main/java/co/sam/shoeshi/client/web/Join.java
+++ src/main/java/co/sam/shoeshi/client/web/Join.java
@@ -9,57 +9,53 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
 import co.sam.shoeshi.bid.serviceImpl.ClientServiceImpl;
 import co.sam.shoeshi.client.service.ClientService;
 import co.sam.shoeshi.client.service.ClientVO;
 
-/**
- * Servlet implementation class Join
- */
 @WebServlet("/join.do")
 public class Join extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Join() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ClientVO vo = new ClientVO();
-		ClientService dao = new ClientServiceImpl();
-		String id = request.getParameter("id");
-		String password = request.getParameter("password");
-//		String password2 = request.getParameter("password2");
-		String name = request.getParameter("name");
-		String address = request.getParameter("address");
-		String tel = request.getParameter("tel");
-		System.out.println(id + "" + password + "" + name + "" + address + "" + tel+""+getInitParameterNames());
-		vo.setClientId(id);
-		vo.setClientPassword(password);
-//		vo.setClientPassword(request.getParameter("password2"));
-		vo.setClientName(name);
-		vo.setClientAddress(address);
-		vo.setClientTel(tel);
-		
-		dao.clientInsert(vo);
-		response.setContentType("text/html; charset=UTF-8");
-		PrintWriter writer = response.getWriter();
-		writer.println("<script>alert('회원가입 완료.'); location.href='http://localhost/example/home.do'</script>"); 
-		writer.close();
+	public Join() {
+		super();
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ClientVO vo = new ClientVO();
+		ClientService dao = new ClientServiceImpl();
+		String saveDir = getServletContext().getRealPath("attech/member/");
+		int sizeLimit = 100 * 1024 * 1024;
+		MultipartRequest multi = new MultipartRequest(request, saveDir, sizeLimit, "UTF-8",
+				new DefaultFileRenamePolicy());
+
+		vo.setClientId(multi.getParameter("clientId"));
+		vo.setClientPassword(multi.getParameter("clientPassword"));
+		vo.setClientName(multi.getParameter("clientName"));
+		vo.setClientTel(multi.getParameter("clientTel"));
+		vo.setClientAddress(multi.getParameter("clientAddress"));
+
+		PrintWriter writer = response.getWriter();
+		
+		if (dao.clientInsert(vo) != 0) {
+			response.setContentType("text/html; charset=UTF-8");
+			writer.println("<script>alert('회원가입 완료.'); location.href='http://localhost/example/home.do'</script>");
+			writer.close();
+		} else {
+			response.setContentType("text/html; charset=UTF-8");
+			writer.println("<script>alert('회원가입 실패.'); location.href='http://localhost/example/home.do'</script>");
+			writer.close();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		doGet(request, response);
 	}
 
