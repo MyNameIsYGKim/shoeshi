e7b4d7a5d253bda47d9029228aa3e6556709342d
admin <admin@DESKTOP-13MN080>
2023-08-22 09:43:08 +0900

    ㅇㅇ

--- src/main/java/co/sam/shoeshi/product/mapper/ProductMapper.java
+++ src/main/java/co/sam/shoeshi/product/mapper/ProductMapper.java
@@ -2,13 +2,15 @@ package co.sam.shoeshi.product.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import co.sam.shoeshi.product.service.ProductVO;
 
 public interface ProductMapper {
 	List<ProductVO>	productSelectList();
-	List<ProductVO>	productSelectList(String key, String val);
+	List<ProductVO>	productSelectList(@Param("key") String key, @Param("val") String val);
 	
-	List<ProductVO>	productSearchList(String key, String val);
+	List<ProductVO>	productSearchList(@Param("key") String key, @Param("val") String val);
 	ProductVO productSelect(ProductVO vo);
 	int productInsert(ProductVO vo);
 	int productUpdate(ProductVO vo);
