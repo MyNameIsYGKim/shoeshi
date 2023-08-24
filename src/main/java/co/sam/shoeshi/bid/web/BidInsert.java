a54fe652fef499e4d3882e5fe68e9bd56563f439
GIRIBOY <wkd3795@hanmail.net>
2023-08-24 17:49:53 +0900

    230824 기리 최종

--- src/main/java/co/sam/shoeshi/bid/web/BidInsert.java
+++ src/main/java/co/sam/shoeshi/bid/web/BidInsert.java
@@ -35,30 +35,42 @@ public class BidInsert extends HttpServlet {
 		vo2.setProductId(Integer.parseInt(request.getParameter("productId")));
 		vo2.setClientId((String) session.getAttribute("clientId"));
 		vo2.setProductSize(Integer.parseInt(request.getParameter("productSize")));
+
 		if (request.getParameter("buttonType").equals("bid")) {
 			vo2.setBidPrice(Integer.parseInt(request.getParameter("bidPrice")));
 			vo2.setBidType(request.getParameter("bidType"));
+
 			int n = dao2.bidInsert(vo2);
 			if (n == 0) {
 				request.setAttribute("alertBid", "입찰등록 실패");
 			} else {
 				request.setAttribute("alertBid", request.getParameter("bidType2") + "입찰 등록");
 			}
-		}else if(request.getParameter("buttonType").equals("deal")) {
-			if(request.getParameter("bidType").equals("BUY")){
-				vo2.setBidType("SELL");
-			}else if(request.getParameter("bidType").equals("SELL")){
-				vo2.setBidType("BUY");
-			}else {
-				vo2.setBidType("");
+		} else if (request.getParameter("buttonType").equals("deal")) {
+			try {
+				if (request.getParameter("bidType").equals("BUY")) {
+					vo2.setBidType("SELL");
+					vo2 = dao2.bidSelectBuy(vo2);
+					vo2.setBidNo(vo2.getBidNo());
+				} else if (request.getParameter("bidType").equals("SELL")) {
+					vo2.setBidType("BUY");
+					vo2 = dao2.bidSelectSell(vo2);
+					vo2.setBidNo(vo2.getBidNo());
+				}
+				int n = dao2.bidDelete(vo2);
+				if (n == 0) {
+					request.setAttribute("alertBid", request.getParameter("bidType2") + " 실패");
+				} else {
+					request.setAttribute("alertBid", request.getParameter("bidType2") + " 완료");
+				}
+			} catch (Exception e) {
+				request.setAttribute("alertBid", "남은 입찰정보가 본인이 등록한 입찰이거나, "+ request.getParameter("bidType2") +"할 입찰정보가 없습니다.");
 			}
-			vo2.setBidPrice(Integer.parseInt(request.getParameter("dealPrice")));
-			int n = dao2.bidDelete(vo2);
 		}
 		ProductService dao = new ProductServiceImpl();
 		HashMap<String, Object> product = new HashMap<String, Object>();
 		ProductVO vo = new ProductVO();
-		vo.setProductId(vo2.getProductId());
+		vo.setProductId(Integer.valueOf(request.getParameter("productId")));
 		product = dao.productJoinSelect(vo);
 		request.setAttribute("p", product);
 
